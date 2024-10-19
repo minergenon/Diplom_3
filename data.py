1840c0a52eb483dc922dec1aafd3d8cf7956c70f
Sergey Palcheh <minergenon@altlinux.org>
2024-10-19 11:37:25 +0600

    Added data.py

---
+++ data.py
@@ -0,0 +1,16 @@
+class UserData:
+    USER_EMAIL = 'testeronchik2099@yandex.ru' # Генерируем через API
+
+    NEW_PASSWORD = 'Ne7Na5Do'
+
+class Urls:
+    BASE_URL = "https://stellarburgers.nomoreparties.site/"
+    ORDER_HISTORY_URL = "account/order-history"
+    LOGIN_URL = "login"
+
+    LIST_ORDER_PAGE = "feed"    # Страница "Лента заказов"
+
+    API_URL = "https://stellarburgers.nomoreparties.site/api/"
+
+    CREATE_USER_ENDPOINT = "auth/register" # POST запрос на создание пользователя
+    DELETE_USER_ENDPOINT = "auth/user"  # метод DELETE
